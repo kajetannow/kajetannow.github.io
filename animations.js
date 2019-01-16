f28319f7ce13874916d5bb2a8e7256016ac97377
unknown <kajetannow>
2019-01-16 22:41:52 +0100

    1st upload

---
+++ animations.js
@@ -0,0 +1,11 @@
+/*var h = document.body.scrollHeight;
+var blob = document.createElement('img');
+blob.className = 'blob';
+blob.id = 'floating';
+blob.src = 'svg/red02.svg'
+var position = blob.style.bottom;
+position = -h + 'px';
+var create = document.querySelector('section#main');
+create.appendChild(blob);
+
+console.log(blob);*/
---
+++ menu.js
@@ -0,0 +1,25 @@
+const body = document.body;
+var o = window.screen.orientation.type;
+var nav = document.createElement('nav');
+var ul = document.createElement('ul');
+if(o == 'landscape-primary' || o== 'landscape-secondary' || o == '90' || o == '270'){
+    nav.appendChild(ul);
+    var sections = document.querySelectorAll('section')
+    console.log(sections);
+
+    for(var i = 0; i < sections.length; i++){
+        var li = document.createElement('li');
+        var a = document.createElement('a');
+        a.href = '#' + sections[i].id;
+        a.innerText = sections[i].querySelector('h1').innerText;
+        if(i==0)
+            a.innerText = 'Strona Główna';
+        li.appendChild(a);
+        ul.appendChild(li);
+    }
+    console.log(nav);
+    body.appendChild(nav)
+
+}else{
+    body.removeChild('nav')
+}
